86419a5fde440cfb81b514955eda1325bda3f606
Léaud Le Bacq <leaudlebacq@jorge.local>
2018-01-03 21:17:09 +0100

    2ndSubmission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions allow to avoid to compute twice the same calculation
 
-## Write a short comment describing this function
+## makeCacheMatrix provides 4 functions as tools for the main function called "cacheSolve"
+## "i" and "y" are two free variables here
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) i <<- inv
+  getinverse <- function() i
+  list(get = get, 
+       set = set, 
+       getinverse = getinverse, 
+       setinverse = setinverse)
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" is the main function that will test if the calculation has already been made in the past
+## If it has, it just has to retrieve the value stocked in the variable "i"
+## Otherwise, it would just calculate it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
+## Following functions are just for documentation
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
